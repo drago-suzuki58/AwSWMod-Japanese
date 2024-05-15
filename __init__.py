a632461ddaa6a0cfe19160a5f1de33c2520c3921
どらすず <dorasuzu58@gmail.com>
2024-05-15 23:06:00 +0900

    feat: __init__.pyを追加

---
+++ __init__.py
@@ -0,0 +1,12 @@
+from modloader.modclass import Mod, loadable_mod
+
+@loadable_mod
+class AWSWMod(Mod):
+    def mod_info(self):
+        return ("AwSWMod-Japanese", "v0.1", "DragoSuzuki58")
+
+    def mod_load(self):
+        pass
+
+    def mod_complete(self):
+        pass
